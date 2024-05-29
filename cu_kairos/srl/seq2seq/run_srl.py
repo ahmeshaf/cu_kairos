3c006f9f54ce7e02e2bf81ffb552127be13fd04a
rehan <ahmeshaf.rehan@gmail.com>
2024-05-29 09:14:26 -0600

    adding model names

--- cu_kairos/srl/seq2seq/run_srl.py
+++ cu_kairos/srl/seq2seq/run_srl.py
@@ -9,13 +9,26 @@ def generate_srl_prompts(sentences, sentences_triggers):
             sentence = (
                 sentence[: trigger[1][0]]
                 + f"[{trigger[0]}]"
-                + sentence[trigger[1][1]:]
+                + sentence[trigger[1][1] :]
             )
             srl_prompt = f"SRL for [{trigger[0]}]: {sentence}"
             yield i, trigger, srl_prompt
 
 
-def srler(tagger_model, srl_model, sentences, batch_size=32):
+def srler(
+    sentences,
+    tagger_model="ahmeshaf/ecb_tagger_seq2seq",
+    srl_model="cu-kairos/propbank_srl_seq2seq_t5_large",
+    batch_size=32,
+):
+    """
+
+    :param tagger_model: Huggingface model name or path. See https://huggingface.co/ahmeshaf/ecb_tagger_seq2seq
+    :param srl_model: Huggingface model name or path. See https://huggingface.co/cu-kairos/propbank_srl_seq2seq_t5_large
+    :param sentences: List[str]
+    :param batch_size: int
+    :return:
+    """
     # get trigger tags
     tagger_pipe = pipeline("text2textgeneration", tagger_model)
 
@@ -34,8 +47,3 @@ def srler(tagger_model, srl_model, sentences, batch_size=32):
 
     # Todo: parse the srls and return offsets
     # TODO: use find_phrase_offsets_fuzzy from ...util
-
-
-
-
-
